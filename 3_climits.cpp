3aa8e32bee2c2dd9317ac94536aba24a1fc95398
zwygo-ash <zwygo@outlook.com>
2024-02-22 21:17:17 +0800

    Create 3_climits.cpp

---
+++ 3_climits.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <climits>
+#include <cfloat>
+
+using namespace std;
+
+int main() {
+
+	cout << "CHAR_MAX: " << CHAR_MAX << endl;
+	cout << "UCHAR_MAX: " << UCHAR_MAX << endl;
+
+	cout << "SHRT_MAX: " << SHRT_MAX << endl;
+	cout << "USHRT_MAX: " << USHRT_MAX << endl;
+
+	cout << "INT_MAX: " << INT_MAX << endl;
+	cout << "UINT_MAX: " << UINT_MAX << endl;
+
+	cout << "LONG_MAX: " << LONG_MAX << endl;
+	cout << "ULONG_MAX: " << ULONG_MAX << endl;
+
+	cout << "LLONG_MAX: " << LLONG_MAX << endl;
+
+	cout << "Minimum int value = " << INT_MIN << endl;
+
+	cout << "Bits per byte = " << CHAR_BIT << endl;
+
+	// for more infor @see "limits.h" & "float.h"
+
+	// sizeof运算符返回类型或变量的大小(字节)
+	cout << "sizeof(long long) = " << sizeof(long long) << endl;
+	cout << "std::numeric_limits<long long>::digits = "
+		<< std::numeric_limits<long long>::digits << endl;
+
+	return 0;
+}
